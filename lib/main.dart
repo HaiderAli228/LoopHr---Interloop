431d708c798950b448200ce106ab2e501c2905aa
HaiderAli228 <flutter2830@gmail.com>
2025-02-27 19:16:20 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:loophr/view/home_page_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePageView(),
+    );
+  }
+}
---
+++ lib/view/home_page_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class HomePageView extends StatelessWidget {
+  const HomePageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
